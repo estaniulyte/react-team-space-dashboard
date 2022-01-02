a256a308646cca06dbfbda572443f396b953bc31
evelinastaniulyte <43463977+estaniulyte@users.noreply.github.com>
2022-01-02 14:19:07 +0200

    Create header component

---
+++ src/components/header/Header.js
@@ -0,0 +1,21 @@
+import React from "react";
+
+import "./header.scss"
+
+const Header = () => {
+  return (
+    <header className="header">
+      <div className="header__logo">
+        Logo
+      </div>
+      <div className="header__tabs">
+        Tabs
+      </div>
+      <div className="header__buttons">
+        Buttons
+      </div>
+    </header>
+  )
+}
+
+export default Header;
