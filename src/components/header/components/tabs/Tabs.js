854537777fca2bbc2431125b3dad0b15ad03d7de
evelinastaniulyte <43463977+estaniulyte@users.noreply.github.com>
2022-01-06 00:08:32 +0200

    Add active tab implementation

--- src/components/header/components/tabs/Tabs.js
+++ src/components/header/components/tabs/Tabs.js
@@ -1,11 +1,14 @@
-import React from "react";
+import React, { useState } from "react";
 import HomeIcon from "assets/icons/home.svg"
 import BookmarkIcon from "assets/icons/bookmark.svg"
 import CompassIcon from "assets/icons/compass.svg"
+import classNames from "classnames";
 
 import "./tabs.scss";
 
 const Tabs = () => {
+  const [isActive, setIsActive] = useState(0);
+
   const tabItems = [
     {
       name: "Dashboard",
@@ -30,11 +33,15 @@ const Tabs = () => {
   return (
     <nav className="tabs">
       <ul className="tabs__list">
-        {tabItems.map((item) => (
-          <li className="tabs__list__item" key={item.link}>
+        {tabItems.map((item, i) => (
+          <button className="tabs__list__item" key={item.link} onClick={() => setIsActive(i)}
+            className={classNames("tabs__list__item", {
+              "tabs__list__item--active": i == isActive,
+            })}
+          >
             <img src={item.imgSrc} alt={item.alt} />
             {item.name}
-          </li>
+          </button>
         ))}
       </ul>
     </nav>
