cfe1a5e38f5b03c4c9036300300454f275c8ccf6
evelinastaniulyte <43463977+estaniulyte@users.noreply.github.com>
2022-01-12 11:57:36 +0200

    Change paths and Active tab handling

--- src/components/header/components/tabs/Tabs.js
+++ src/components/header/components/tabs/Tabs.js
@@ -1,6 +1,4 @@
-import React, { useState } from "react";
 import { NavLink } from "react-router-dom";
-import classNames from "classnames";
 
 import HomeIcon from "assets/icons/home.svg"
 import BookmarkIcon from "assets/icons/bookmark.svg"
@@ -9,24 +7,22 @@ import CompassIcon from "assets/icons/compass.svg"
 import "./tabs.scss";
 
 const Tabs = () => {
-  const [isActive, setIsActive] = useState(0);
-
   const tabItems = [
     {
       name: "Dashboard",
-      link: "/react-team-space-dashboard",
+      link: "/",
       imgAlt: "Home icon",
       imgSrc: HomeIcon,
     },
     {
       name: "Reservations",
-      link: "/react-team-space-dashboard/reservations",
+      link: "/reservations",
       imgAlt: "Bookmark logo",
       imgSrc: BookmarkIcon,
     },
     {
       name: "Eat-out",
-      link: "/react-team-space-dashboard/eat-out",
+      link: "/eat-out",
       imgAlt: "Compass logo",
       imgSrc: CompassIcon,
     },
@@ -35,21 +31,18 @@ const Tabs = () => {
   return (
     <nav className="tabs">
       <ul className="tabs__list">
-        {tabItems.map((item, i) => (
+        {tabItems.map((item) => (
           <NavLink
             key={item.name}
             to={item.link}
-            className="tabs__list__item"
+            className={({ isActive }) =>
+              isActive
+                ? "tabs__list__item tabs__list__item--active"
+                : "tabs__list__item"
+            }
           >
-            <button
-              onClick={() => setIsActive(i)}
-              className={classNames("tabs__list__item", {
-                "tabs__list__item--active": i === isActive,
-              })}
-            >
               <img src={item.imgSrc} alt={item.alt} />
                {item.name}
-            </button>
           </NavLink>
         ))}
       </ul>
