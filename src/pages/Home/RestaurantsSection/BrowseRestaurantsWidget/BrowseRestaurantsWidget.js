60af7c2249bf6ab8caa22c1de269487cfbca02d7
evelinastaniulyte <43463977+estaniulyte@users.noreply.github.com>
2022-01-12 11:58:21 +0200

    Add content and styling of BrowseRestaurantsWidget component

--- src/pages/Home/RestaurantsSection/BrowseRestaurantsWidget/BrowseRestaurantsWidget.js
+++ src/pages/Home/RestaurantsSection/BrowseRestaurantsWidget/BrowseRestaurantsWidget.js
@@ -1,11 +1,16 @@
+import Button from "components/Button";
 import React from "react";
+import { Link } from "react-router-dom";
 
 import "./browse-restaurants-widget.scss"
 
 const BrowseRestaurantsWidget = () => {
   return (
-    <div>
-      Widget
+    <div className="widget">
+      <h3 className="widget__header">View all your favourite lunch spots and more</h3>
+      <Link to="/eat-out">
+        <Button size="md" text="Browse list" />
+      </Link>
     </div>
   );
 };
