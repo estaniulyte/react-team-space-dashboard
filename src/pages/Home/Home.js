7fd0091a3a4737e8a8f81073c97b9286e29f53fe
evelinastaniulyte <43463977+estaniulyte@users.noreply.github.com>
2022-01-23 22:49:10 +0200

    Get data for cards

--- src/pages/Home/Home.js
+++ src/pages/Home/Home.js
@@ -1,9 +1,29 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 
-import "./home.scss"
 import RestaurantsSection from "./RestaurantsSection";
 
+import { restaurantsData } from 'assets/data/restaurants';
+import { userData } from 'assets/data/userData';
+
+import "./home.scss"
+
 const Home = () => {
+  const [userLikedReastaurants, setUserLikedReastaurants] = useState([]);
+
+  useEffect(() => {
+    if (userData && restaurantsData) {
+      const likedRestaurantsIDs = userData[0].liked.restaurants;
+      const likedRestaurantsIDsArray = likedRestaurantsIDs.map(
+        ({ id }) => id
+      );
+
+      const likedRestaurants = restaurantsData.filter((restaurant) =>
+        likedRestaurantsIDsArray.includes(restaurant.id)
+      );
+      setUserLikedReastaurants(likedRestaurants);
+    }
+  }, []);
+
   return (
     <main>
       <RestaurantsSection />
